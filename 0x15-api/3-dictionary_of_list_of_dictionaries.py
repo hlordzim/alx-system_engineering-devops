66dc8aa9e85db0ca7e18d3e670479b4753b7d0ef
hlordzim <hlordzim.mh@gmail.com>
2024-05-21 17:26:55 +0000

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,58 +3,33 @@
 
 import json
 import requests
+import sys
 
 
-def fetch_all_todos():
-    try:
-        # Fetch all users information
-        users_url = "https://jsonplaceholder.typicode.com/users"
-        users_response = requests.get(users_url)
-        users_response.raise_for_status()
-        users_data = users_response.json()
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users"
 
-        all_todos = {}
+    resp = requests.get(url)
+    Users = resp.json()
 
-        for user in users_data:
-            employee_id = user['id']
-            username = user['username']
+    users_dict = {}
+    for user in Users:
+        USER_ID = user.get('id')
+        USERNAME = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)
+        url = url + '/todos/'
+        resp = requests.get(url)
 
-            # Fetch TODO list for the current employee
-            todos_url = (
-                f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-            )
-            todos_response = requests.get(todos_url)
-            todos_response.raise_for_status()
-            todos_data = todos_response.json()
-
-            # Prepare data for current employee
-            tasks = []
-            for task in todos_data:
-                task_info = {
-                    "username": username,
-                    "task": task['title'],
-                    "completed": task['completed']
-                }
-                tasks.append(task_info)
-
-            all_todos[employee_id] = tasks
-
-        # Write data to JSON file
-        json_filename = "todo_all_employees.json"
-        with open(json_filename, 'w') as json_file:
-            json.dump(all_todos, json_file, indent=4)
-
-        print(f"Data exported to {json_filename} successfully.")
-
-    except requests.RequestException as e:
-        print(f"HTTP Request failed: {e}")
-    except KeyError:
-        print("Error: Unexpected response structure")
-    except ValueError:
-        print("Error: Invalid JSON response")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-if __name__ == "__main__":
-    fetch_all_todos()
+        tasks = resp.json()
+        users_dict[USER_ID] = []
+        for task in tasks:
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            users_dict[USER_ID].append({
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME
+            })
+            """A little Something"""
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(users_dict, f)
