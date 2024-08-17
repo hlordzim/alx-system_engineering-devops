c6cf557723c426aa284e65364d503d2292a45225
hlordzim <hlordzim.mh@gmail.com>
2024-08-17 15:10:06 -0700

    web-stack_debbuging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# Fixes bad phpp extensions to php in the WordPress file wp-settings.php
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
 exec { 'fix-wordpress':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/bin'],
-  unless  => 'grep -q phpp /var/www/html/wp-settings.php'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
